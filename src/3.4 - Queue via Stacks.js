622ac4d5fb5f72dd3fd9ad97e3c74a9e7930ada6
Robb Marsee <robb.marsee@gamil.com>
2017-03-07 16:22:31 -0800

    add dev dependencies, add test file, move ch1 code to one file

--- src/3.4 - Queue via Stacks.js
+++ src/3.4 - Queue via Stacks.js
@@ -8,4 +8,3 @@ const queueViaStacks = () => {
   // once complete the first item in Stack #1 will be at the end of Stack #2 and can be pushed off and saved
   // then each item in Stack #2 will be poped off and pushed into Stack #1
   // then the item that was saved could be returned as the item that is dequed...aka the first item to be added to the queue
-  
---
+++ src/index.test.js
@@ -0,0 +1,43 @@
+/* eslint-disable */ 
+
+const expect = require('chai').expect;
+const Code = require('./Code');
+
+describe('Chapter One', function() {
+  
+  it('1.1 - Is Unique', function() {
+    expect(true).to.be.true;
+  })
+
+  it('1.2 - Check Permutation', function() {
+    expect(true).to.be.true;
+  })
+
+  it('1.3 - URLify', function() {
+      expect(true).to.be.true;
+    })
+  
+  it('1.4 - Palindrome Permutation', function() {
+      expect(true).to.be.true;
+    })
+  
+  it('1.5 - One Away', function() {
+      expect(true).to.be.true;
+    })
+  
+  it('1.6 - String Compression', function() {
+      expect(true).to.be.true;
+    })
+  
+  it('1.7 - Rotate Matrix', function() {
+      expect(true).to.be.true;
+    })
+
+  it('1.8 - Zero Matrix', function() {
+      expect(true).to.be.true;
+    })
+
+  it('1.9 - String Rotation', function() {
+      expect(true).to.be.true;
+    })
+})
