c971b63330ef1cfe82b3ba5a683373813ec5e11a
Robb Marsee <robb.marsee@gamil.com>
2017-03-10 08:17:45 -0800

    update files for recursion problems

---
+++ src/chapter8.js
@@ -0,0 +1,29 @@
+// === CHAPTER 8 === //
+
+// 8.1 - Triple Step
+
+// 8.2 - Robot in a Grid
+
+// 8.3 - Magic Index
+
+// 8.4 - Power Set
+
+// 8.5 - Recursive Multiply
+
+// 8.6 - Towers of Hanoi
+
+// 8.7 - Permutations without Dups
+
+// 8.8 - Permutations with Dups
+
+// 8.9 - Parens
+
+// 8.10 - Paint Fill
+
+// 8.11 - Coins
+
+// 8.12 - Eight Queens
+
+// 8.13 - Stack of Boxes
+
+// 8.14 - Boolean Evaluation
